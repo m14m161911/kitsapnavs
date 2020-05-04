608f57394ea342a27d77e0477a86183bcbd0879a
Jon Van Slyke <m14m161911@gmail.com>
2020-05-03 23:40:38 -0700

    modified index.html, index.js to build website from javascript

---
+++ index.js
@@ -0,0 +1,66 @@
+function textSelect (sel) {
+    if (sel = 1) {
+        document.write("<div class='grid-container'>");
+        document.write("     <div class='item1'>");
+        document.write("         <img src='KitsapNavigators.jpg' width= 500px alt='Kitsap Navigators Logo'>");
+        document.write("</div>");
+        document.write("<div class='item2'>");
+        document.write("    <div class='btn'><button onclick='textSelect(1)'>Navigators Calling</button></div>");
+        document.write("    <div class='btn'><button onclick='textSelect(2)'>Navigators Values</button></div>");
+        document.write("    <div class='btn'><button onclick='textSelect(3)'>Navigators Vision</button></div>");
+        document.write("</div>")''
+        document.write("<div class='item3'>");
+        document.write("    <div class='square'> <h1>Navigator Calling</h1> </div>");
+        document.write("         <div class='square'> ");
+        document.write("To know Christ, make him known, and help others to do the same.");
+        document.write("         </div>");
+        document.write("    </div>");
+        document.write("</div>"); 
+        document.write("<div class='item4'></div>");
+        document.write("<div class='item5'></div>");
+    }
+    else if (sel = 2) {
+        document.write("        <div class='item3'>");
+        document.write("             <div class='square'> <h1>Navigator Calling</h1> </div>");
+        document.write("                  <div class='square'> ");
+        document.write("The passion to know, love and become like Jesus Christ. Phil. 3:10, Matt. 10:24,25 <br>");
+        document.write("The truth and sufficiency of the Scriptures for the whole of life. II Tim 3:16,17, II Pet. 1:3,4 <br>");
+        document.write("The transforming power of the Gospel. Rom. 1:16 <br>");
+        document.write("The leading and empowering of the Holy Spirit. Acts 1:8, II Cor. 3:18 <br>");
+        document.write("Expectant faith and persevering prayer rooted in the promises of God. Rom 4:19,20, Heb. 11:6 <br>");
+        document.write("The dignity and value of every person. Is. 51:1,2; 60:22 <br>");
+        document.write("Love and grace expressed among us in community. John 13:34,35,Col. 3:12-17 <br>");
+        document.write("Families and relational networks in discipling the nations. I Thess. 1 <br>");
+        document.write("Interdependent relationships in the Body of Christ in advancing the Gospel. Phil 1:3-14 <br>");
+        document.write("                  </div>");
+        document.write("             </div>");
+        document.write("        </div>");   
+    }
+    else {
+        document.write("        <div class='item3'>");
+        document.write("             <div class='square'> <h1>Navigator Calling</h1> </div>");
+        document.write("                  <div class='square'> ");
+        document.write("We see a vital movement of the Gospel, fueled by prevailing prayer, flowing freely through relational networks and ");
+        document.write("out into the nations. Workers for the Kingdom are next door to everywhere!  What characterizes this movement? A heart for ");
+        document.write("the whole person ... climates of grace ... compassion for the vulnerable and broken ... sacrificial unity embracing diversity ... ");
+        document.write("cultural relevance and sensitivity ... interdependence with others in the wider family of God ... transformed men and women, ");
+        document.write("fragrant with humility and the aroma of Christ. They are marked by a deep engagement with and obedience to the Scriptures as ");
+        document.write("the Word of God. They believe the promises of God. Both personally and in committed communities, they seek to know and pursue ");
+        document.write("the purposes of God. Laborers and leaders are emerging, with an increasing passion for Jesus Christ. They demonstrate faith and ");
+        document.write("courage as they live and move among their friends and families. As spiritual parents, they model authenticity and relevance. ");
+        document.write("Ordinary people, in many walks of life, are joyfully leading integrated lives. They live as fruitful insiders among the lost. ");
+        document.write("There is perseverance in the face of hardship and suffering. Around the world, many are coming to faith. As they become ");
+        document.write("established in discipleship, some grow to be foundational for further generations. The Gospel spreads naturally and powerfully, ");
+        document.write("as believers share Christ ... life upon life ... family to family. Crossing cultures into new cities and nations, teams of mobile ");
+        document.write("pioneers intentionally proclaim and embody the good news of Jesus Christ, in such a way that transformed communities multiply. ");
+        document.write("These communities are bringing joy and hope to their surrounding environments as relationships are healed and justice increases. ");
+        document.write("Indeed, the lost and unreached burn in their hearts, as they move the Gospel into the nations. The leaders of this movement, ");
+        document.write("developed and empowered for God’s service, live out a growing commitment to Christlikeness. They are dependent upon the Holy Spirit. ");
+        document.write("New generations of leaders are emerging, rooted incarnationally in their local and national contexts. An international leadership ");
+        document.write("community brings focus, alignment and energy to their movement. These leaders are clearly committed to long-term impact in ");
+        document.write("generational ministry.");
+        document.write("                  </div>");
+        document.write("             </div>");
+        document.write("        </div>");   
+    }
+}
